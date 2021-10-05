b1133d79f46c835ecd5c66eba849704164f3b8e3
kjy1101 <amelia1023@aver.com>
2021-10-06 05:32:29 +0900

    question random pick, today, yesterday, past 3 days

--- musicdiary/models.py
+++ musicdiary/models.py
@@ -7,8 +7,7 @@ from account.models import User
 class Question(models.Model):
     id = models.AutoField(primary_key=True, null=False, blank=False)
     question_content = models.CharField(max_length=150) #질문
-    user = models.ForeignKey(User, null=True, blank=True, on_delete=CASCADE) #질문등록자
-    pub_date = models.DateTimeField(auto_now_add=True) #질문등록날짜
+    released_date = models.DateField(null=True, blank=True)
 
     def __str__(self):
         return self.question_content
