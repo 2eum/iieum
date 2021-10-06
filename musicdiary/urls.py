458884af3939b4eee26d7ec54ee845348e6eff57
kjy1101 <amelia1023@aver.com>
2021-10-06 22:20:20 +0900

    clean backend code

--- musicdiary/urls.py
+++ musicdiary/urls.py
@@ -1,25 +1,16 @@
 from django.urls import path
 from django.conf.urls import include, url
-from .views import MusicdiaryViewSet, QuestionViewSet, SearchView, MyPageView, RandomQuestion, PastQuestion#, TodayQuestion, YesterdayQuestion
+from .views import *
 from rest_framework import routers
 
 router = routers.DefaultRouter()
 router.register('api/musicdiary', MusicdiaryViewSet)
 router.register('api/question', QuestionViewSet)
 
-musicdiary_detail = MusicdiaryViewSet.as_view({
-    'get': 'retrieve',
-    'put': 'update',
-    'delete': 'destroy'
-})
-
 urlpatterns = [
     url('', include(router.urls)),
-    path('api/musicdiary/<int:pk>/',musicdiary_detail), 
     path('api/spotify/', SearchView.as_view()), #음악검색
     path('api/mypage/', MyPageView.as_view()), # 마이페이지
     path('api/question/random', RandomQuestion.as_view()), # 질문 랜덤돌리기(오늘의 질문 선택)
-    #path('api/today-question/', TodayQuestion.as_view()), # 오늘의 질문
-    #path('api/yesterday-question/', YesterdayQuestion.as_view()), # 어제의 질문
-    path('api/question/past', PastQuestion.as_view()), # 2,3,4일 전 질문
+    path('api/question/past', PastQuestion.as_view()), # 0,1,2,3,4일 전 질문 불러오기
 ]
--- musicdiary/views.py
+++ musicdiary/views.py
@@ -1,8 +1,7 @@
-from django.shortcuts import render
 from rest_framework import viewsets
 from .serializers import MusicdiarySerializer, QuestionSerializer
 from .models import Musicdiary, Question
-from rest_framework.authentication import SessionAuthentication, BasicAuthentication, TokenAuthentication 
+from rest_framework.authentication import TokenAuthentication 
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from .permissions import IsOwnerOrReadOnly
 from rest_framework.response import Response
@@ -16,21 +15,21 @@ from django.db.models import Max
 import random
 from datetime import datetime, timedelta
 
-# 전체 글 보여주기(Viewset)
+
+
+# 전체 글 보여주기
 class MusicdiaryViewSet(viewsets.ModelViewSet): 
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticatedOrReadOnly,IsOwnerOrReadOnly]
     queryset = Musicdiary.objects.all().order_by('-pub_date')
     serializer_class = MusicdiarySerializer
-
     # serializer.save() 재정의
-    def perform_create(self, serializer): #쓰기
-        serializer.save(user=self.request.user)#, track_title=self.request.data['track_title'], track_artist=self.request.data['track_artist'], track_album_cover=self.request.data['track_album_cover'], track_audio=self.request.data['track_audio'])
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
 
-    #def perform_update(self, serializer): #수정하기
-        #serializer.save(user=self.request.user, track_title=self.request.data['track_title'], track_artist=self.request.data['track_artist'], track_album_cover=self.request.data['track_album_cover'], track_audio=self.request.data['track_audio'])
 
-# 음악 검색(View)
+
+# 음악 검색
 class SearchView(APIView):
     @csrf_exempt
     def post(self, request):
@@ -40,17 +39,11 @@ class SearchView(APIView):
         client_credentials_manager = SpotifyClientCredentials(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
         sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
         results = sp.search(search_word)
-        """
-        검색 결과 parsing index
-        for track in results.tracks.items:
-            노래제목: track.name
-            가수이름: track.artists.0.name
-            앨범아트: track.album.images.0.url
-            미리듣기: track.preview_url
-        """
         return Response({"Search word": search_word, "Results": results})
 
-# 마이페이지(View)
+
+
+# 마이페이지
 class MyPageView(APIView):
     authentication_classes = [TokenAuthentication]
     permission_classes = (IsAuthenticatedOrReadOnly,IsOwnerOrReadOnly )
@@ -73,17 +66,20 @@ class MyPageView(APIView):
             }
             serializer = MusicdiarySerializer(data=request.data, context=serializer_context)
             if serializer.is_valid(): #유효성 검사
-                serializer.save(user=self.request.user)#, track_title=self.request.data['track_title'], track_artist=self.request.data['track_artist'], track_album_cover=self.request.data['track_album_cover'], track_audio=self.request.data['track_audio']) # 저장
+                serializer.save(user=self.request.user)
                 return Response(serializer.data, status=status.HTTP_201_CREATED)
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         else:
             return Response({"detail":"Please login"})
+
+
 
 class QuestionViewSet(viewsets.ModelViewSet): 
     authentication_classes = [TokenAuthentication]
     permission_classes = (IsAuthenticatedOrReadOnly,IsOwnerOrReadOnly )
     queryset = Question.objects.all() 
     serializer_class = QuestionSerializer
+
 
 
 # 질문 랜덤돌리기(하루에 한번 호출해서 오늘의 질문 선택)
@@ -102,30 +98,8 @@ class RandomQuestion(APIView):
                 return Response(serializer_class.data)
 
 
-# 오늘의 질문 보여주기 (HOME 1)
-"""class TodayQuestion(APIView):
-    @csrf_exempt
-    def get(self, request):
-        today_question = Question.objects.filter(released_date=datetime.today().date()).first()
-        if today_question:
-            serializer_context = {'request': request,}
-            serializer_class = QuestionSerializer(today_question, many=False, context=serializer_context)
-            return Response(serializer_class.data)
 
-
-# 어제의 질문 보여주기 (HOME 2)
-class YesterdayQuestion(APIView):
-    @csrf_exempt
-    def get(self, request):
-        yesterday = datetime.today().date() - timedelta(1)
-        yesterday_question = Question.objects.filter(released_date=yesterday).first()
-        if yesterday_question:
-            serializer_context = {'request': request,}
-            serializer_class = QuestionSerializer(yesterday_question, many=False, context=serializer_context)
-            return Response(serializer_class.data)"""
-
-
-# 2,3,4일 전 질문 보여주기 (HOME 4)
+# 최근 5일 간의 질문 불러오기 (오늘, 어제, 2,3,4일 전)
 class PastQuestion(APIView):
     @csrf_exempt
     def get(self, request):
