5a09dfca1022b8d399c8cae40fc1e60fe297268c
kjy1101 <amelia1023@aver.com>
2021-11-10 05:37:20 +0900

    endpoint question&post 1

--- post/urls.py
+++ post/urls.py
@@ -14,5 +14,11 @@ urlpatterns = [
     path('api/question/random', RandomQuestion.as_view()), # 질문 랜덤돌리기(오늘의 질문 선택)
     path('api/question/past', PastQuestion.as_view()), # 0,1,2,3,4일 전 질문 불러오기
     #path('api/question/lastweek-popular', LastWeekPopularQuestion.as_view()), # 지난주 인기질문
-    path('api/like/<int:post_id>/', LikeToggle.as_view()) # 좋아요 기능
+    path('api/like/<int:post_id>/', LikeToggle.as_view()), # 좋아요 기능
+    path('api/questionlist', QuestionList.as_view()),
+    path('api/getonepost', GetOnePost.as_view()),
+    path('api/postlist-user', PostList_user.as_view()),
+    path('api/postlist-date', PostList_date.as_view()),
+    path('api/postlist-user-date', PostList_user_date.as_view()),
+    path('api/postlist-question', PostList_question.as_view()),
 ]
--- post/views.py
+++ post/views.py
@@ -14,18 +14,15 @@ from django.db.models import Max
 import random
 from datetime import datetime, timedelta
 
-
-# 전체 글 보여주기
+# 전체 글
 class PostViewSet(viewsets.ModelViewSet): 
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticatedOrReadOnly,IsOwnerOrReadOnly]
     queryset = Post.objects.all().order_by('-pub_date')
     serializer_class = PostSerializer
-    
     # serializer.save() 재정의
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
-
 
 # 음악 검색
 class SearchView(APIView):
@@ -39,9 +36,7 @@ class SearchView(APIView):
         results = sp.search(search_word)
         return Response({"Search word": search_word, "Results": results})
 
-
-
-# 마이페이지
+# 마이페이지 => 추후 삭제
 class MyPageView(APIView):
     authentication_classes = [TokenAuthentication]
     permission_classes = (IsAuthenticatedOrReadOnly,IsOwnerOrReadOnly )
@@ -57,34 +52,25 @@ class MyPageView(APIView):
         else:
             return Response({"detail":"Please login"})
 
-
-
 class QuestionViewSet(viewsets.ModelViewSet): 
     authentication_classes = [TokenAuthentication]
     permission_classes = (IsAuthenticatedOrReadOnly,IsOwnerOrReadOnly )
     queryset = Question.objects.all() 
     serializer_class = QuestionSerializer
 
-
-
 # 질문 랜덤돌리기(하루에 한번 호출해서 오늘의 질문 선택)
 class RandomQuestion(APIView):
     @csrf_exempt
     def get(self, request):
         max_id = Question.objects.all().aggregate(max_id=Max("id"))['max_id']
-        # 쿨타임이 오늘 날짜로 등록됐던 질문들은 다시 null로 바꿈(쿨타임 끝남, 다시 랜덤 범위에 포함) & 에러 처리 겸용
+        # 오늘 날짜로 등록됐던 질문들은 다시 null로 바꿈(에러 처리)
         free_question = Question.objects.filter(released_date=datetime.today().date())
         for fq in free_question:
             fq.released_date = None
             fq.save()
-        # 어제 질문은 일주일 뒤 날짜로 바꿈(쿨타임 시작)
-        deleted_question = Question.objects.filter(released_date=datetime.today().date() - timedelta(1))
-        for dq in deleted_question:
-            dq.released_date = dq.released_date + timedelta(weeks=1)
-            dq.save()
         while True:
             pk = random.randint(1, max_id)
-            # released_date가 null이면서 pk가 해당 랜덤값인 질문들
+            # released_date가 null인 것들 중에 찾음
             random_question = Question.objects.filter(released_date=None).filter(pk=pk).first()
             if random_question:
                 serializer_context = {'request': request,}
@@ -93,49 +79,17 @@ class RandomQuestion(APIView):
                 serializer_class = QuestionSerializer(random_question, many=False, context=serializer_context)
                 return Response(serializer_class.data)
 
-
-
-# 최근 5일 간의 질문 불러오기
+# 최근 5일 간의 질문 불러오기(최신순) => 추후 삭제
 class PastQuestion(APIView):
     @csrf_exempt
     def get(self, request):
         enddate = datetime.today().date();
         startdate = enddate - timedelta(4)
-
         pastday_question = Question.objects.filter(released_date__range=[startdate, enddate]).order_by('-released_date')
-
         if pastday_question:
             serializer_context = {'request': request,}
             serializer_class = QuestionSerializer(pastday_question, many=True, context=serializer_context)
             return Response(serializer_class.data)
-
-
-
-# 지난주 인기질문 (그 질문에 그 날 작성된 글의 개수)
-"""class LastWeekPopularQuestion(APIView):
-    @csrf_exempt
-    def get(self, request):
-        startdate = datetime.today().date()-timedelta(7);
-        enddate = datetime.today().date()- timedelta(1);
-
-        lastweek_question = Question.objects.filter(released_date__range=[startdate, enddate])
-
-        if lastweek_question:
-            Post_count = -1
-            Post_max = -1
-            popular_pk = 0
-            for last_q in lastweek_question:
-                date_range1 = datetime.combine(last_q.released_date, datetime.min.time())
-                date_range2 = datetime.combine(last_q.released_date, datetime.max.time())
-                Post_count = Post.objects.filter(question=last_q.id).filter(pub_date__range=[date_range1, date_range2]).count()
-                if Post_count > Post_max:
-                    Post_max = Post_count
-                    popular_pk = last_q.id
-
-            popular_question = Question.objects.filter(pk=popular_pk).first()
-            serializer_context = {'request': request,}
-            serializer_class = QuestionSerializer(popular_question, many=False, context=serializer_context)
-            return Response(serializer_class.data)"""
 
 # 좋아요 기능 
 class LikeToggle(APIView):
@@ -153,3 +107,114 @@ class LikeToggle(APIView):
         serializer_context = {'request': request,}
         serializer_class = PostSerializer(like_list, many=False, context=serializer_context)
         return Response(serializer_class.data)
+
+
+"""========================================= Endpoint 추가 및 수정 ========================================="""
+
+
+# 특정 날짜/개수 범위 내의 질문리스트 (req: 날짜, 개수제한 / res: 질문 리스트)
+class QuestionList(APIView):
+    @csrf_exempt
+    def get(self, request):
+        startdate = request.data['startdate']   # 이날부터
+        enddate = request.data['enddate']       # 이날까지
+        limit = request.data['limit']           # 최대 몇개
+        if int(limit) == 0: # 한계없음
+            questionlist = Question.objects.filter(released_date__range=[startdate, enddate]).order_by('-released_date')
+        else:
+            questionlist = Question.objects.filter(released_date__range=[startdate, enddate]).order_by('-released_date')[:int(limit)]
+        if questionlist:
+            serializer_context = {'request': request,}
+            serializer_class = QuestionSerializer(questionlist, many=True, context=serializer_context)
+            return Response(serializer_class.data)
+        else:
+            return Response({"detail":"No question exist"})
+
+
+# 타겟 글 객체 하나 (req: 글 id, res: 글 객체)
+class GetOnePost(APIView):
+    @csrf_exempt
+    def get(self, request):
+        pk = request.data['pk']
+        post = Post.objects.filter(pk=pk).first()
+        if post:
+            serializer_context = {'request': request,}
+            serializer_class = PostSerializer(post, many=False, context=serializer_context)
+            return Response(serializer_class.data)
+        else:
+            return Response({"detail":"No post exist"})
+
+
+# 타겟 유저의 글목록 (req: 유저 id(숫자값), 개수 제한 res: 해당 유저의 글 목록)
+class PostList_user(APIView):
+    @csrf_exempt
+    def get(self, request):
+        pk = request.data['pk']
+        limit = request.data['limit']
+        if int(limit) == 0: # 한계없음
+            postlist = Post.objects.filter(user=pk).order_by('-pub_date')
+        else:
+            postlist = Post.objects.filter(user=pk).order_by('-pub_date')[:int(limit)]
+        if postlist:
+            serializer_context = {'request': request,}
+            serializer_class = PostSerializer(postlist, many=True, context=serializer_context)
+            return Response(serializer_class.data)
+        else:
+            return Response({"detail":"No post exist"})
+
+
+# 특정 날짜 범위 내의 글목록, 유저 구분X (req: 날짜, 개수 제한 / res: 해당 기간 동안의 글 목록)
+class PostList_date(APIView):
+    @csrf_exempt
+    def get(self, request):
+        startdate = request.data['startdate']   # 이날부터
+        enddate = request.data['enddate']       # 이날까지
+        limit = request.data['limit']
+        if int(limit) == 0: # 한계없음
+            postlist = Post.objects.filter(pub_date__range=[startdate, enddate]).order_by('-pub_date')
+        else:
+            postlist = Post.objects.filter(pub_date__range=[startdate, enddate]).order_by('-pub_date')[:int(limit)]
+        if postlist:
+            serializer_context = {'request': request,}
+            serializer_class = PostSerializer(postlist, many=True, context=serializer_context)
+            return Response(serializer_class.data)
+        else:
+            return Response({"detail":"No post exist"})
+
+
+# 타겟 유저의 특정 날짜 범위 내의 글 목록 (req: 유저 아이디, 날짜 범위, 개수 제한 / res: 해당 기간 동안 유저의 글 목록)
+class PostList_user_date(APIView):
+    @csrf_exempt
+    def get(self, request):
+        pk = request.data['pk']                 # 이 사람의
+        startdate = request.data['startdate']   # 이날부터
+        enddate = request.data['enddate']       # 이날까지의 글
+        limit = request.data['limit']
+        if int(limit) == 0: # 한계없음
+            postlist = Post.objects.filter(user=pk).filter(pub_date__range=[startdate, enddate]).order_by('-pub_date')
+        else:
+            postlist = Post.objects.filter(user=pk).filter(pub_date__range=[startdate, enddate]).order_by('-pub_date')[:int(limit)]
+        if postlist:
+            serializer_context = {'request': request,}
+            serializer_class = PostSerializer(postlist, many=True, context=serializer_context)
+            return Response(serializer_class.data)
+        else:
+            return Response({"detail":"No post exist"})
+
+
+# 질문에 달린 글 목록 (req: 질문 id, 개수 제한 / res: 그 질문에 달린 글 목록)
+class PostList_question(APIView):
+    @csrf_exempt
+    def get(self, request):
+        pk = request.data['pk']
+        limit = request.data['limit']
+        if int(limit) == 0: # 한계없음
+            postlist = Post.objects.filter(question=pk).order_by('-pub_date')
+        else:
+            postlist = Post.objects.filter(question=pk).order_by('-pub_date')[:int(limit)]
+        if postlist:
+            serializer_context = {'request': request,}
+            serializer_class = PostSerializer(postlist, many=True, context=serializer_context)
+            return Response(serializer_class.data)
+        else:
+            return Response({"detail":"No post exist"})
