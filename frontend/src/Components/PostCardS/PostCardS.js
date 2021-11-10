a21e24bd1e786e9847ee29c85e7b047015958c1f
koowoy <uk960214@gmail.com>
2021-11-11 00:21:07 +0900

    change import name to S

--- frontend/src/Components/PostCardS/PostCardS.js
+++ frontend/src/Components/PostCardS/PostCardS.js
@@ -1,28 +1,26 @@
 import React from "react";
-import * as e from "./PostCardS.elements";
-
+import * as S from "./PostCardS.elements";
 
 const PostCardS = () => {
   return (
-    <e.PostCardArea>
-      <e.TopArea>
-        <e.Author>Mayzel</e.Author>
-      </e.TopArea>
-      <e.MusicArea>
-        <e.MusicCover src="https://image.bugsm.co.kr/album/images/500/40591/4059180.jpg"></e.MusicCover>
-        <e.MusicInfoArea>
-          <e.MusicTitleWrapper>
-            <e.MusicTitle>KNOCK(With 박문치dddd)</e.MusicTitle>
-          </e.MusicTitleWrapper>
-          <e.MusicArtist>권진아</e.MusicArtist>
-        </e.MusicInfoArea>
-      </e.MusicArea>
-      <e.TitleArea>
-        <e.ContentTitle>나란히 누워 별을 셀 때</e.ContentTitle>
-      </e.TitleArea>
-    </e.PostCardArea>
-  
-    );
+    <S.PostCardArea>
+      <S.TopArea>
+        <S.Author>Mayzel</S.Author>
+      </S.TopArea>
+      <S.MusicArea>
+        <S.MusicCover src="https://imagS.bugsm.co.kr/album/images/500/40591/4059180.jpg"></S.MusicCover>
+        <S.MusicInfoArea>
+          <S.MusicTitleWrapper>
+            <S.MusicTitle>KNOCK(With 박문치dddd)</S.MusicTitle>
+          </S.MusicTitleWrapper>
+          <S.MusicArtist>권진아</S.MusicArtist>
+        </S.MusicInfoArea>
+      </S.MusicArea>
+      <S.TitleArea>
+        <S.ContentTitle>나란히 누워 별을 셀 때</S.ContentTitle>
+      </S.TitleArea>
+    </S.PostCardArea>
+  );
 };
 
 export default PostCardS;
