c3fae94f0d189f6878d25ae4a9a5f88440900aec
hyojinki <hyojinki99@gmail.com>
2021-11-24 03:18:21 +0900

    email design

--- frontend/src/globalStyles.js
+++ frontend/src/globalStyles.js
@@ -6,70 +6,11 @@ const GlobalStyle = createGlobalStyle`
 
 // import web font
 
-  //noto serif kr
-  // font-family: 'Noto Serif KR'
-  // font-weight로 조절
+//noto serif kr
+// font-family: 'Noto Serif KR'
+// font-weight로 조절
 /* @import url('https://cdn.rawgit.com/kattergil/NotoSerifKR-Web/76eb9ebf/stylesheet/NotoSerif-Web.css'); */
 
-
-
-
-@font-face {
-  font-family: "Pretendard-Thin";
-  src: url("https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Thin.woff")
-    format("woff");
-  font-weight: 100;
-  font-style: normal;
-}
-@font-face {
-  font-family: "Pretendard-ExtraLight";
-  src: url("https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-ExtraLight.woff")
-    format("woff");
-  font-weight: 200;
-  font-style: normal;
-}
-@font-face {
-  font-family: "Pretendard-Light";
-  src: url("https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Light.woff")
-    format("woff");
-  font-weight: 300;
-  font-style: normal;
-}
-@font-face {
-  font-family: "Pretendard-Regular";
-  src: url("https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff")
-    format("woff");
-  font-weight: 400;
-  font-style: normal;
-}
-@font-face {
-  font-family: "Pretendard-Medium";
-  src: url("https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Medium.woff")
-    format("woff");
-  font-weight: 500;
-  font-style: normal;
-}
-@font-face {
-  font-family: "Pretendard-SemiBold";
-  src: url("https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-SemiBold.woff")
-    format("woff");
-  font-weight: 600;
-  font-style: normal;
-}
-@font-face {
-  font-family: "Pretendard-Bold";
-  src: url("https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Bold.woff")
-    format("woff");
-  font-weight: 700;
-  font-style: normal;
-}
-@font-face {
-  font-family: "Pretendard-ExtraBold";
-  src: url("https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-ExtraBold.woff")
-    format("woff");
-  font-weight: 800;
-  font-style: normal;
-}
 
 // defaults
 * {
@@ -98,6 +39,8 @@ const GlobalStyle = createGlobalStyle`
   &::-webkit-scrollbar-thumb:active{
     background: rgba(0,0,0,.9);
   }
+
+  wordbreak: keep-all;
 }
 
 .fa {
@@ -120,16 +63,15 @@ h1, h2, h3 {
   font-weight: 500;
 }
 
+* {
+  wordbreak: keep-all;
+}
+
 `;
 
 export const CardShadow = "8px 8px 8px 1px rgba(0, 0, 0, .15)";
 
 export const ButtonShadow = "2px 2px 4px 0 rgba(0, 0, 0, .25)";
-
-export const BannerContainer = styled.section`
-  padding: 3rem;
-  text-align: center;
-`;
 
 export const BoldSpan = styled.span`
   color: ${colors.iiPurple};
