f7bacf75f9b86298e547441bcef8ff45ca0991c2
koowoy <uk960214@gmail.com>
2021-12-24 22:07:45 +0900

    fix: footer 이메일 주소 위치 변경

--- frontend/src/Containers/Footer/Footer.js
+++ frontend/src/Containers/Footer/Footer.js
@@ -1,21 +1,21 @@
-import React from "react";
-import * as S from "./Footer.elements";
-import spotifyIcon from "../../Components/SearchedItem/Spotify_Icon.png";
+import React from 'react';
+import * as S from './Footer.elements';
+import spotifyIcon from '../../Components/SearchedItem/Spotify_Icon.png';
 
 const Footer = () => {
   return (
     <>
       <S.FooterContainer>
+        <p>문의사항과 건의사항은 iieum.ms.official@gmail.com로 보내주세요.</p>
         <S.CopyRight>
           © 2021 <strong>IIEUM</strong>, All Rights Reserved
         </S.CopyRight>
-        <p>문의사항과 건의사항은 iieum.ms.official@gmail.com로 보내주세요.</p>
         <p>
-          {" "}
-          Search & Data Powered by Spotify{" "}
+          {' '}
+          Search & Data Powered by Spotify{' '}
           <span>
             <img width="16px" src={spotifyIcon} />
-          </span>{" "}
+          </span>{' '}
         </p>
       </S.FooterContainer>
     </>
