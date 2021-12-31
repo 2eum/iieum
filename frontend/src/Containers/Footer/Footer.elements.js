e4d8e3d1be0614e77c68d4005412fdfb4393744e
koowoy <uk960214@gmail.com>
2021-12-31 23:29:49 +0900

    style: footer에 padding 변경

--- frontend/src/Containers/Footer/Footer.elements.js
+++ frontend/src/Containers/Footer/Footer.elements.js
@@ -1,9 +1,8 @@
-import styled from "styled-components";
-import * as colors from "../../styles/Colors";
+import styled from 'styled-components';
+import * as colors from '../../styles/Colors';
 
 export const FooterContainer = styled.section`
-  padding-top: 6rem;
-  padding-bottom: 5rem;
+  padding: 5rem 2rem;
 
   border-top: 0.5px solid ${colors.iiBrown};
   opacity: 0.7;
